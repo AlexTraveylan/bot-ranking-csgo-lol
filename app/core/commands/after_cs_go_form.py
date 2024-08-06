ebba84602773f21e8071a55950fd1680d4cfde30
AlexTraveylan <monsieurdemares@gmail.com>
2024-08-07 01:28:39 +0200

    Refactor AfterCsGoForm to use steam_id instead of player_id

--- app/core/commands/after_cs_go_form.py
+++ app/core/commands/after_cs_go_form.py
@@ -12,19 +12,19 @@ from app.core.database.services.discord_member import DiscordMemberService
 class AfterCsGoForm:
     def __init__(
         self,
-        player_id: str,
+        steam_id: str,
         discord_author_id: int,
         discord_author_name: str,
         session: Session,
     ) -> None:
-        self.player_id = player_id
+        self.steam_id = steam_id
         self.discord_author_id = discord_author_id
         self.discord_author_name = discord_author_name
         self.session = session
         self.is_member_exist: Literal["get", "create"]
 
     def __repr__(self) -> str:
-        return f"AfterCsGoForm(player_id={self.player_id}, discord_author_id={self.discord_author_id}, discord_author_name={self.discord_author_name}, is_member_exist={self.is_member_exist})"
+        return f"AfterCsGoForm(steam_id={self.steam_id}, discord_author_id={self.discord_author_id}, discord_author_name={self.discord_author_name}, is_member_exist={self.is_member_exist})"
 
     def get_or_create_discord_member(self) -> DiscordMember:
         is_member_exist, member = DiscordMemberService.get_or_create(
@@ -40,28 +40,28 @@ class AfterCsGoForm:
         )
 
         is_existing = any(
-            self.player_id == account.steam_id for account in cs_go_accounts
+            self.steam_id == account.steam_id for account in cs_go_accounts
         )
 
         if is_existing is True:
             raise UniqueConstraintException(
-                f"Player id {self.player_id} already exists in the database"
+                f"Player id {self.steam_id} already exists in the database"
             )
 
         return None
 
     def create(self, member: DiscordMember) -> CsGoAccount:
-        stats = get_player_info(self.player_id)
+        stats = get_player_info(self.steam_id)
 
         db_account = CsGoAccount(
-            steam_id=self.player_id,
+            steam_id=self.steam_id,
             game_name=stats.name,
             discord_member_id=member.id,
         )
 
         account_created = CsGoAccountService.create(self.session, db_account)
 
-        stats_data = stats.to_dict_for_db(self.player_id)
+        stats_data = stats.to_dict_for_db(self.steam_id)
         db_stats = CsGoStats(**stats_data, csgo_account_id=account_created.id)
 
         CsGoAccountService.create(self.session, db_stats)
